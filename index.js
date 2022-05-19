52986ddc0d43a06db28d7722fa7061cbff467cec
Tauqeer Ahmad <hellotauqeer@gmail.com>
2022-05-19 20:28:17 +0530

    working fine after public

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/button', (req, res) => {
-    res.sendFile(__dirname + '/button.html');
-  })
+    res.sendFile(__dirname + '/public/button.html');
+    console.log(__dirname + '/public/button.html');
+})
+
+app.use(express.static('public'));
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
